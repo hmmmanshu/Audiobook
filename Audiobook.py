eb5ff9bce3cffc833b703fa5f6f3343a44526596
Himanshu Negi <51152254+Bot-7037@users.noreply.github.com>
2020-12-09 01:28:18 +0530

    Update Audiobook.py

--- Audiobook.py
+++ Audiobook.py
@@ -1,10 +1,12 @@
-import pyttsx3 #Python text to speech
-import PyPDF2 #Python PDF reader
-book = open('oop.pdf', 'rb')
+import pyttsx3 
+import PyPDF2 
+file_name = input("Enter book name with path and suitable extension)
+book = open(file_name, 'rb')
 pdfReader = PyPDF2.PdfFileReader(book)
 pages = pdfReader.numPages
 speaker = pyttsx3.init()
-for num in range(7, pages):
+n = int(input("Enter page no. to start with"))
+for num in range(n, pages):
     page = pdfReader.getPage(num)
     text = page.extractText()
     speaker.say(text)
