e0bc1dcb6d3984dfd27ac36852f159fea3c719b5
Himanshu Negi <51152254+Bot-7037@users.noreply.github.com>
2020-12-09 09:34:06 +0530

    Update Audiobook.py

--- Audiobook.py
+++ Audiobook.py
@@ -1,19 +1,19 @@
 import pyttsx3 
 import PyPDF2 
-file_name = input("Enter book name with path and suitable extension)
+file_name = input("Enter book name with path and suitable extension")
 book = open(file_name, 'rb')
 pdfReader = PyPDF2.PdfFileReader(book)
 pages = pdfReader.numPages
 speaker = pyttsx3.init()
 n = int(input("Enter page no. to start with"))
 choice = 'N'
-while(choice=='N'){
+while(choice=='N'):
     print("Current rate of speaking = ",str(speaker.getProperty('rate')))
     speaker.setProperty('rate', int(input("Enter a rate or enter 125")))
     speaker.say("Hii .... Is this speed suitable to you ? Y/N",end=": ")
     speaker.runAndWait()
     choice = input()
-}
+
 engine.setProperty('voice', voices[int(input("Enter 0 for male and  1 for female voice"))].id)
 
 for num in range(n, pages):
