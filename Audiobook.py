ad1fb93994b2e96237801a18532c92c273b9d8b4
Himanshu Negi <51152254+Bot-7037@users.noreply.github.com>
2020-12-09 09:32:06 +0530

    Update Audiobook.py

--- Audiobook.py
+++ Audiobook.py
@@ -6,6 +6,16 @@ pdfReader = PyPDF2.PdfFileReader(book)
 pages = pdfReader.numPages
 speaker = pyttsx3.init()
 n = int(input("Enter page no. to start with"))
+choice = 'N'
+while(choice=='N'){
+    print("Current rate of speaking = ",str(speaker.getProperty('rate')))
+    speaker.setProperty('rate', int(input("Enter a rate or enter 125")))
+    speaker.say("Hii .... Is this speed suitable to you ? Y/N",end=": ")
+    speaker.runAndWait()
+    choice = input()
+}
+engine.setProperty('voice', voices[int(input("Enter 0 for male and  1 for female voice"))].id)
+
 for num in range(n, pages):
     page = pdfReader.getPage(num)
     text = page.extractText()
